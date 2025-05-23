a5147b76a70303ec0c7ee67ded72a6b3477c1d6e
Parker Conn <parkerconn@gmail.com>
2025-05-22 20:37:23 -0400

    fix mobile header nav

--- pages/utils/useHeader.ts
+++ pages/utils/useHeader.ts
@@ -2,4 +2,21 @@ import header from '../components/header.html?raw'
 
 export function useHeader() {
   document.querySelector<HTMLDivElement>('#header')!.innerHTML = header;
+
+  document.getElementById('menu-toggle')!.addEventListener('click', (event) => {
+    const mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu!.classList.toggle('hidden');
+    event.stopPropagation();
+  });
+
+  // Close menu when clicking outside of header
+  document.addEventListener('click', (event) => {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const header = document.getElementById('legacy-boxing-header');
+
+    if (mobileMenu && !mobileMenu.classList.contains('hidden') &&
+      header && !header.contains(event.target as Node)) {
+      mobileMenu.classList.add('hidden');
+    }
+  });
 }
